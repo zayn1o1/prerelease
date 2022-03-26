fb812458325e732015f7251d58f80f03c53448cd
zayn1o1 <102375707+zayn1o1@users.noreply.github.com>
2022-03-26 13:17:57 +0500

    Update prerelease.py

--- prerelease.py
+++ prerelease.py
@@ -6,83 +6,75 @@ Costfor1day = [20.00,12.00,16.00,60.00,15.00]
 ExtraAttraction = ["1.lion feeding","2.penguin feeding","3.evening bbq"]
 AttractionCost = [2.50,2.00,5.00]
 Costfor2days = [30.00,18.00,24.00,90.00,22.50]
-for count in range (5):
+'''for count in range (5):
     print ("Ticket type =",TicketType[count])
     print ("Cost for a day =",Costfor1day[count],)
     print ("Cost for two days =",Costfor2days[count])
-    for count in range (3):
-        print ("Extra Attraction", ExtraAttraction[count], "=", AttractionCost[count])
     print ("--------------------")
+for count in range (3):
+ print ("Extra Attraction", ExtraAttraction[count], "=", AttractionCost[count])
+ print ("--------------------")
 next
 print ("You can book for the following days:")
 for count in range (7):
     bookingdays = x.day + count + 1 
     if bookingdays > 31:
         bookingdays = bookingdays - 31
-    print (bookingdays)
+    print (bookingdays)'''
+
 ###TASK 2
 tickettype = int(0)
 totalcost = int(0)
 bookingid = int(0)
 addedcost = int(0)
-tickettype = int(input("Please input the ticket that you want to buy(1 to 5):"))
-while tickettype > 5 or tickettype < 1:
-        tickettype = int(input("Please enter a valid ticket type:"))
+tickettype = int(input("Please input the ticket that you want to buy(1 to 5 or -1 to end booking):")) 
+while tickettype > 5 or tickettype < -1 or tickettype == 0:
+     tickettype = int(input("Please enter a valid ticket type:"))
 notickets = int(input("Please enter the number of tickets you wish to buy:"))
 while tickettype == 5 and notickets < 6:
-    print ("A group ticket has to be bought for atleast 6 people")
-    tickettype = int(input("Please input the ticket that you want to buy(1 to 5):"))   
+     print ("A group ticket has to be bought for atleast 6 people")
+     tickettype = int(input("Please input the ticket that you want to buy(1 to 5):"))
 bookingday = int(input("Confirm booking for 1= 1 Day or 2= 2 Days:"))
 if bookingday > 2 or bookingday < 1:
-    bookingday = int(input("Please enter a valid booking day:"))
+     bookingday = int(input("Please enter a valid booking day:"))
 if bookingday == 1:
-     totalcost = notickets * Costfor1day[tickettype - 1]
+    totalcost = notickets * Costfor1day[tickettype - 1]
 elif bookingday == 2:
-     totalcost = notickets * Costfor2days[tickettype - 1] 
-print ("total cost",totalcost)
+    totalcost = notickets * Costfor2days[tickettype - 1] 
 attractionchoice = int(input("Would you like an extra attraction?1 for yes,2 for no:"))
 if attractionchoice == 1:
     extraattraction = int(input("Please input the attraction you want to add (1 to 3):"))
     ch = int(input("Would you like to add another attraction?1 for yes, 2 for no:"))                      
     while ch != 2:
-        extraattraction = int(input("Please input the attraction you want to add (1 to 3):"))                        
+            extraattraction = int(input("Please input the attraction you want to add (1 to 3):"))                        
     if bookingday == 1 and extraattraction == 3:
-        print ("BBQ is reserved for 2 day bookings only")
+            print ("BBQ is reserved for 2 day bookings only")
 if attractionchoice == 1:
-    addedcost = totalcost + AttractionCost[extraattraction - 1]
+        addedcost = totalcost + AttractionCost[extraattraction - 1]
 elif attractionchoice == 2:
-    addedcost = totalcost
+        addedcost = totalcost
 bookingid = bookingid + 1
 print ("Your bookingid: ", bookingid)
 print ("Your ticket type =",TicketType[tickettype - 1])
 print ("Number of tickets bought =",notickets)
 print ("Total cost =", addedcost)
 
-
-
-
-            
-                      
-   
-
-
-    
-    
-    
-
-
-
-
-    
-
-       
-
-
-
-
-
-
-
+###TASK 3
+count = int(0)
+if tickettype == 1 or tickettype == 3 and notickets >= 6:
+    print ("Please consider getting group tickets for best value")
+while (tickettype == 1 or tickettype == 3) > 1 and (tickettype == 2) >= 3:
+    if (tickettype == 1) >= 2:
+        (tickettype == 1) == (tickettype == 1) - 2
+    elif (tickettype == 3) >= 2:
+        (tickettype == 3) == (tickettype == 3)-2
+    (tickettype == 2) == (tickettype == 2) - 3
+    count = count + 1
+if (count > 0):
+    print ("Buying",count,"family tickets is better than individual tickets")
+    print ("Following tickets should be bought seperately (0=none)")
+    print ("Number of Adults       Number of Children    Number of Seniors")
+    print (tickettype == 1     ,   tickettype == 2     ,   tickettype == 3)
 
 
     
